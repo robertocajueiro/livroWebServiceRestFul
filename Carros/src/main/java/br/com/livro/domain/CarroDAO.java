83cd3b45f2c75108383dfe3a5cb4b317f1253f9d
robertocajueiro <beto.cajueiro@gmail.com>
2022-01-27 21:06:02 -0300

    update CarroDAO

--- Carros/src/main/java/br/com/livro/domain/CarroDAO.java
+++ Carros/src/main/java/br/com/livro/domain/CarroDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.eclipse.jdt.internal.compiler.flow.FinallyFlowContext;
 
 public class CarroDAO extends BaseDAO {
 	
@@ -119,4 +122,83 @@ public class CarroDAO extends BaseDAO {
 		c.setTipo(rs.getString("tipo"));
 		return c;
 	}
+	
+	public void save(Carro c) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try {
+			conn = getConnection();
+			if(c.getId() == null) {
+				stmt = conn.prepareStatement("insert into carro (nome,descricao,url,"
+						+ "url_foto,url_video,latitude,longitude,tipo) VALUES(?,?,?,?,?,?,?)",
+						Statement.RETURN_GENERATED_KEYS);
+			} else {
+				stmt = conn.prepareStatement("update carro set nome=?,descricao=?ur_foto=?,"
+						+ "url_video=?,latitude=?,longitude=?,tipo=?, where id=?");
+			}
+			
+			stmt.setString(1, c.getNome());
+			stmt.setString(2, c.getDesc());
+			stmt.setString(3, c.getUrlFoto());
+			stmt.setString(4, c.getUrlVideo());
+			stmt.setString(5, c.getLatitude());
+			stmt.setString(6, c.getLongitude());
+			stmt.setString(7, c.getTipo());
+			if(c.getId() != null) {
+			
+			// Update
+			
+			stmt.setLong(8, c.getId());
+		}
+		
+		int count = stmt.executeUpdate();
+		if(count == 0) {
+			throw new SQLException("Erro inserir o carro");
+		}
+		
+		// Se inseriu, ler o id auto incremento
+		if (c.getId() == null) {
+			Long id = getGeneratedId(stmt);
+			c.setId(id);
+		  }
+		} finally {
+			if(stmt != null) {
+				stmt.close();
+		}
+			if(conn != null) {
+				conn.close();
+			}
+		}	
+	}
+
+	// id gerado com o campo auto incremento
+public static Long getGeneratedId(Statement stmt) throws SQLException {
+	ResultSet rs = stmt.getGeneratedKeys();
+	if(rs.next()) {
+		Long id = rs.getLong(1);
+		return id;
+	}
+	return 0L;
+ }
+
+public boolean delete(Long id) throws SQLException {
+	Connection conn = null;
+	PreparedStatement stmt = null;
+	try {
+		conn = getConnection();
+		stmt = conn.prepareStatement("delete from carro where id=?");
+		stmt.setLong(1, id);
+		int count = stmt.executeUpdate();
+		boolean ok = count > 0;
+		return ok;
+	} finally {
+		if(stmt != null) {
+			stmt.close();
+		}
+		if(conn != null) {
+			conn.close();
+		}
+	  }
+    }
 }
+
